1498493e0fc905a81ac792f024ad6ed787ba7e85
anush <anushasree@gmail.com>
2022-12-16 15:14:49 -0500

    adding addition program

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,2 +1,9 @@
 public class FirstProgram {
+   static  int i=10;
+   static int j=20;
+   static int k=i+j;
+     public static void main(String args[]){
+         System.out.println(k);
+
+    }
 }
